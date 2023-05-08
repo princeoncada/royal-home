9520612663fb2dd2d4d2b8534bf547ace638f2f5
Prince Oncada <prince.oncada@gmail.com>
2023-05-08 02:43:42 -0400

    inital commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public/'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res){
+    
+});
+
+
+app.listen(port, function() {
+    console.log('listening on port ' + port);
+});
