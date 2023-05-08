89c8de189d7d1ebc965b8a9742b0c6bfd2f50d33
KuuShyn <magalonakaj@gmail.com>
2023-05-08 15:36:57 +0800

    adding the stuff

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public/'));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public/"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(req, res){
-    res.render('index.ejs')
+app.get("/", function (req, res) {
+  res.render("edit.ejs");
 });
 
-
-app.listen(port, function() {
-    console.log('listening on port ' + port);
+app.listen(port, function () {
+  console.log("listening on port " + port);
 });
